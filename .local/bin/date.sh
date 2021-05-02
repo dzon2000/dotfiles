958f4429f91b17842a0e8c0e0af05d72dabf14b8
dzon2000 <dzon2000@protonmail.com>
2021-05-02 08:58:27 +0200

    Changing picom to use st and date format (monday as a first day of week and hour without AM/PM indicator)

--- .local/bin/date.sh
+++ .local/bin/date.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 case $BUTTON in
-    1) dunstify -h string:x-dunst-stack-tag:calendar "Calendar" "$(cal -3)";;
+    1) dunstify -h string:x-dunst-stack-tag:calendar "Calendar" "$(cal -3 --monday)";;
 esac
 
-date=$(date '+%b %d (%a) %H:%M%p')
+date=$(date '+%b %d (%a) %H:%M')
 echo -e $date
