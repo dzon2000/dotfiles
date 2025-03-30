8c640945f4a1dbc5b340cb84465253211172f2d5
dzon2000 <dzon2000@protonmail.com>
2025-03-30 23:09:38 +0200

    Neovim setup

---
+++ .config/init.lua
@@ -0,0 +1,17 @@
+-- Tabs and spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+require("config.lazy")
+
+-- :hi Normal guibg=NONE ctermbg=NONE
+
+-- Config
+vim.opt.swapfile = false
+vim.g.backgroud = "light"
+
+-- Neotree
+vim.keymap.set('n', '<leader>nt', ':Neotree filesystem reveal left<CR>')
+vim.diagnostic.config({ virtual_text = true })
